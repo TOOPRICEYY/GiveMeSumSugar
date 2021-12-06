de8b5ca4a45579966c16b8bca0cf1bb298d87739
Ryan T <rtruong@umich.edu>
2021-12-06 18:41:45 -0500

    gabe price is crippled

--- BinarySearchTree_tests.cpp
+++ BinarySearchTree_tests.cpp
@@ -5,8 +5,40 @@
 
 
 TEST(test_stub) {
-    // Add your tests here
-    ASSERT_TRUE(true);
+    BinarySearchTree<int> bruh;
+    bruh.insert(5);
+    ASSERT_TRUE(bruh.size()==1);
+    bruh.insert(3);
+    bruh.insert(7);
+    bruh.insert(2);
+    bruh.insert(4);
+    bruh.insert(6);
+    bruh.insert(8);
+    bruh.insert(9);
+    ASSERT_TRUE(bruh.height() == 4);
+    ASSERT_TRUE(bruh.find(9) != bruh.end());
+    ASSERT_TRUE(bruh.find(2) != bruh.end());
+    ASSERT_TRUE(bruh.find(1) == bruh.end());
+    ASSERT_TRUE(*bruh.min_element() == 2);
+    ASSERT_TRUE(*bruh.begin() == 2);
+    ASSERT_TRUE(*bruh.min_greater_than(2)==3);
+    ASSERT_TRUE(bruh.size() == 8);
+    ASSERT_TRUE(bruh.check_sorting_invariant());
+
+    BinarySearchTree<int> aye(bruh);
+    ASSERT_FALSE(aye.empty());
+    aye.insert(1);
+    ostringstream ayeYO;
+    aye.traverse_inorder(ayeYO);
+    ASSERT_EQUAL(ayeYO.str(),"1 2 3 4 5 6 7 8 9 ");
+    ostringstream ayeWHAT;
+    *aye.find(9) = 10;
+    aye.traverse_preorder(ayeWHAT);
+    ASSERT_EQUAL(ayeWHAT.str(),"5 3 2 1 4 7 6 8 10 ");
+    BinarySearchTree<int>::Iterator it = aye.begin();
+    ASSERT_TRUE(*++++it == 3);
+    it = aye.find(10);
+    ASSERT_TRUE(++it == aye.end());
 }
 
 TEST_MAIN()
