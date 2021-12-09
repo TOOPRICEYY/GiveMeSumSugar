8aa61982051b9320decafd333aa600d4361b285c
Gabriel Price <starfinderjetpacks@gmail.com>
2021-12-08 20:56:46 -0500

    sdf

--- main.cpp
+++ main.cpp
@@ -14,13 +14,13 @@ using namespace std;
 
 
 vector<map<string, string>> input_csv(istream&);
-void debug_func(bool , pair<map<string, map<string,double>>,
-map<string, map<string,double>>> &,  map<string,double> &);
+void debug_func(bool , pair<map<string, map<string,int>>,
+map<string, map<string,int>>> &,  map<string,int> &);
 double prob_calc(string , const vector<string> &,
-pair<map<string, map<string,double>>, map<string, map<string,double>>> &, 
-map<string,double> &);
-double prob_calc_one_word(string , string ,  pair<map<string, map<string,double>>,
-map<string, map<string,double>>> &,  map<string,double> &);
+pair<map<string, map<string,int>>, map<string, map<string,int>>> &, 
+map<string,int> &);
+double prob_calc_one_word(string , string ,  pair<map<string, map<string,int>>,
+map<string, map<string,int>>> &,  map<string,int> &);
 
 int main(int argc, char * argv[]) {
 cout.precision(3);
@@ -48,9 +48,9 @@ vector<map<string, string>> parsed = input_csv(fi);
 
 //for( auto & val : parsed){ cout<<val["tag"]<<", "<<val["content"]<<endl;}
 
-map<string, map<string,double>> vocab;
-map<string, map<string,double>> categories;
-map<string,double> cat_counts;
+map<string, map<string,int>> vocab;
+map<string, map<string,int>> categories;
+map<string,int> cat_counts;
 
 
 if(debug) cout << "training data:"<<endl;
@@ -61,7 +61,7 @@ for(auto & val : parsed){ //input all parsed into vocab
     ++cat_counts[val["tag"]];
     ss << val["content"];
     string a;
-    map<string, double> words_to_add; // impliments to stop duplicating additions
+    map<string, int> words_to_add; // impliments to stop duplicating additions
     while(ss>>a) {words_to_add[a]=1;}
     for(auto m : words_to_add){++(vocab[m.first][val["content"]]); ++(categories[val["tag"]][m.first]);}
     if(debug) cout << "  label = " << val["tag"] << ", content = " << val["content"] << endl;
@@ -71,8 +71,8 @@ cout<<"trained on " << i << " examples" << endl;
 if(debug)cout<<"vocabulary size = " << vocab.size()<<endl;
 cout<<endl;
 
-pair<map<string, map<string,double>>,
-map<string, map<string,double>>> bundle = {vocab, categories};
+pair<map<string, map<string,int>>,
+map<string, map<string,int>>> bundle = {vocab, categories};
 
 debug_func(debug,bundle,cat_counts); // prints all log probs for each category
 
@@ -98,9 +98,10 @@ for(auto & val : parsed){ //input all parsed into vocab
     while(ss>>a) words.push_back(a);
 
 
-    double lowest = -1000;
+    double lowest = -100000;
     string winer = "";
     for(auto & cat : cat_counts){
+        if(cat.first == "total") continue;
         double temp = prob_calc(cat.first,words,bundle,cat_counts);
         if(temp>lowest){
             winer = cat.first;
@@ -126,15 +127,15 @@ cout << "performance: " <<correct<<" / "<<i<<" posts predicted correctly"<<endl;
 
 
 
-void debug_func(bool debug_me, pair<map<string, map<string,double>>,
-map<string, map<string,double>>> &bundle,  map<string,double> &cat_counts){
+void debug_func(bool debug_me, pair<map<string, map<string,int>>,
+map<string, map<string,int>>> &bundle,  map<string,int> &cat_counts){
     if(!debug_me) return;
 
     cout<<"classes:"<<endl;
     for(auto n : cat_counts){
         if(n.first=="total") continue;
         cout <<"  "<<n.first<<", "<<n.second<<" examples, log-prior = "<<
-        log(n.second/cat_counts["total"])<<endl;
+        log(double(n.second)/cat_counts["total"])<<endl;
     }
 
 
@@ -148,31 +149,32 @@ map<string, map<string,double>>> &bundle,  map<string,double> &cat_counts){
     }
     cout<<endl;
 }
-double add_up_counts(map<string,double> m){
-    unsigned int t = 0;
+double add_up_counts(map<string,int> m){
+    double t = 0;
     for(auto n : m){
         t+= n.second;
     }
     return t;
 }
 
-double prob_calc_one_word(string category, string word,  pair<map<string, map<string,double>>,
-map<string, map<string,double>>> &bundle,  map<string,double> &cat_counts){
-    if(add_up_counts(bundle.first[word])==0) return log(1/cat_counts["total"]);// if word doesnt show up in training
-    if(bundle.second[category][word]==0) return log(add_up_counts(bundle.first[word])/cat_counts["total"]); //shows up in training but not in this category
+double prob_calc_one_word(string category, string word,  pair<map<string, map<string,int>>,
+map<string, map<string,int>>> &bundle,  map<string,int> &cat_counts){
+    if(add_up_counts(bundle.first[word])==0) return log(1.0/cat_counts["total"]);// if word doesnt show up in training
+    if(bundle.second[category][word]==0) return log(double(add_up_counts(bundle.first[word]))/cat_counts["total"]); //shows up in training but not in this category
     //cout<<" "<<bundle.second[category][word]/cat_counts[category]<<" ";
-    return log(bundle.second[category][word]/cat_counts[category]);
+    return log(double(bundle.second[category][word])/cat_counts[category]);
     
 }
 
 
 double prob_calc(string category, const vector<string> &words,
- pair<map<string, map<string,double>>, map<string, map<string,double>>> &bundle, // can make more efficent by generating a map to refer to for all words
-map<string,double> &cat_counts){
-    double total = log(cat_counts[category]/cat_counts["total"]);
-   
-    for(string word : words){
-        total+=prob_calc_one_word(category,word,bundle,cat_counts);
+ pair<map<string, map<string,int>>, map<string, map<string,int>>> &bundle, // can make more efficent by generating a map to refer to for all words
+map<string,int> &cat_counts){
+    double total = log(double(cat_counts[category])/cat_counts["total"]);
+    map<string, int> words_to_add; // impliments to stop duplicating additions
+    for(auto & word : words) {words_to_add[word]=1;}
+    for(auto & word : words_to_add){
+        total+=prob_calc_one_word(category,word.first,bundle,cat_counts);
     }
 
     return total;
